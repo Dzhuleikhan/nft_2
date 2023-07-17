b71a4afc27358faea1e21b0cd9d5275242e226f8
dzhuleikhan <dzhuleikhan@gmail.com>
2023-07-18 00:09:58 +0400

    Fixes form text files

--- js/main.js
+++ js/main.js
@@ -135,7 +135,7 @@ if (amountDrop) {
       let num = Number(e.target.textContent);
       amountDropBtn.setAttribute("value", num);
       amountDrop.classList.remove("active");
-      amountDropBtn.querySelector("input").value = num;
+      // amountDropBtn.querySelector("input").value = num;
 
       if (amountDropBtn.value >= 1) {
         document.querySelector(".mint-free-btn").style.display = "block";
@@ -166,129 +166,5 @@ if (walletTabBtns) {
       document.querySelector(`.wallet-tab-content-${tab}`).style.display =
         "grid";
     });
-  });
-}
-
-// Select bg
-const itemsBg = document.querySelectorAll(".item-select-bg");
-const itemsBg1 = document.querySelectorAll(".item-select-1");
-const itemsBg2 = document.querySelectorAll(".item-select-2");
-
-const defaultBg = "./img/green-bg.png";
-const defaultItem = "./img/placeholder.png";
-
-itemsBg.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    let bg = e.target.getAttribute("data-bg");
-    let bgId = e.target.getAttribute("data-id");
-    let nftBg = document
-      .getElementById("bacground")
-      .setAttribute("data-id", bgId);
-    document.getElementById("choose-item-1").classList.add("active");
-    document.getElementById("choose-item-1-bg").setAttribute("src", bg);
-    document.getElementById("bacground").setAttribute("src", bg);
-  });
-});
-
-if (document.querySelector(".choose-bg-remove")) {
-  document.querySelector(".choose-bg-remove").addEventListener("click", (e) => {
-    e.preventDefault();
-    document.getElementById("choose-item-1").classList.remove("active");
-    document.getElementById("bacground").setAttribute("src", defaultBg);
-  });
-}
-
-// Select item1
-
-itemsBg1.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    let item = e.target.getAttribute("data-item");
-    let item1 = e.target.getAttribute("data-id");
-    document.getElementById("item1").setAttribute("data-id", item1);
-    document.getElementById("choose-item-2").classList.add("active");
-    document.getElementById("item1").setAttribute("src", item);
-  });
-});
-if (document.querySelector(".choose-item1-remove")) {
-  document
-    .querySelector(".choose-item1-remove")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      document.getElementById("choose-item-2").classList.remove("active");
-      document.getElementById("item1").setAttribute("src", defaultItem);
-    });
-}
-
-// Select item2
-itemsBg2.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    let item = e.target.getAttribute("data-item");
-    let item2 = e.target.getAttribute("data-id");
-    document.getElementById("item2").setAttribute("data-id", item2);
-    document.getElementById("choose-item-3").classList.add("active");
-    document.getElementById("item2").setAttribute("src", item);
-  });
-});
-
-if (document.querySelector(".choose-item2-remove")) {
-  document
-    .querySelector(".choose-item2-remove")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      document.getElementById("choose-item-3").classList.remove("active");
-      document.getElementById("item2").setAttribute("src", defaultItem);
-    });
-}
-
-// Modal
-const approve = document.querySelector(".approve");
-const overlay = document.querySelector(".overlay");
-const overlayClose = document.querySelector(".modal-close");
-
-// Data
-
-const attr = {
-  main_nft_id: "new",
-  attributes: {
-    first_item: "a",
-    second_item: "b",
-    third_item: "c",
-  },
-};
-
-let nftMainId = document.getElementById("alien");
-let nftBg = document.getElementById("bacground");
-let nftItem1 = document.getElementById("item1");
-let nftItem2 = document.getElementById("item2");
-
-if (approve) {
-  approve.addEventListener("click", () => {
-    overlay.classList.add("is-open");
-  });
-
-  overlayClose.addEventListener("click", () => {
-    overlay.classList.remove("is-open");
-  });
-
-  overlay.addEventListener("click", (e) => {
-    if (e.target === overlay) {
-      overlay.classList.remove("is-open");
-    }
-  });
-
-  setTimeout(() => {
-    approve.classList.remove("disabled");
-  }, 2000);
-
-  approve.addEventListener("click", () => {
-    let nftMainImgId = nftMainId.getAttribute("data-id");
-    let nftBgId = nftBg.getAttribute("data-id");
-    let nftItemFirst = nftItem1.getAttribute("data-id");
-    let nftItemSecond = nftItem2.getAttribute("data-id");
-    attr.main_nft_id = nftMainImgId;
-    attr.attributes.first_item = nftBgId;
-    attr.attributes.second_item = nftItemFirst;
-    attr.attributes.third_item = nftItemSecond;
-    console.log(attr);
   });
 }
